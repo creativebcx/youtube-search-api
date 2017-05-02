ac38775d30f97b12f850033c54b5acdae2d1e30e
creativebcx <ericdonley1@gmail.com>
2017-05-01 22:03:15 -0600

    final

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-df
+var youtubeSearch = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  var query = {
+    q: searchTerm,
+    r: 'json',
+    key: 'AIzaSyDfR_RyvzZNVQXC3TTAwtUx8a55aWG8rU8',
+    part: 'snippet'
+
+  }
+  $.getJSON(youtubeSearch, query, callback);
+}
+
+function displayYoutubeSearchData(data) {
+  var resultElement = '';
+  if (data.items) {
+    data.items.forEach(function(item) {
+     resultElement += '<img src="' + item.snippet.thumbnails.default.url + '"/>';
+    });
+  }
+  //use <a element, open and close in different
+  else {
+    resultElement += '<p>No results</p>';
+  }
+  
+  $('.js-search-results').html(resultElement);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(event) {
+    event.preventDefault();
+    var query = $(this).find('.js-query').val();
+    getDataFromApi(query, displayYoutubeSearchData);
+  });
+}
+
+$(function(){watchSubmit();});
